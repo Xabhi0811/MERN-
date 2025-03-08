99f59c4fb6e831460dfcb001e90b380c7844bdaf
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-03-08 14:44:50 +0530

    Create api.js
    In this i was calling api

---
+++ api.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container"></div>
+    <button onclick="getanimal()">getanimal</button>
+</body>
+<script>
+    
+    async function getanimal()
+    {
+       const response = await fetch("https://catfact.ninja/fact")
+        const final = await response.json()
+        console.log(final);
+    }
+</script>
+</html>
