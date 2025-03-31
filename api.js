c87378f9b5d8e91d2c299a7a9caa85c27a47dbdb
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-03-31 20:32:54 +0530

    Update api.js

--- api.js
+++ api.js
@@ -13,7 +13,6 @@
 <script>
     async function getanimal()
     {
-        
      const response = await fetch("https://catfact.ninja/fact")
     const final = await response.json()
     console.log(final);  
